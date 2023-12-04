787cb4943e8ccdfa14e4a7c6ba0011790f1efb6f
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-12-04 09:04:57 +0100

    Crados metodos de la clase Vehiculo

--- scripts/main.js
+++ scripts/main.js
@@ -3,64 +3,95 @@
  * **GitHub:**
  */
 
-const autoContainer = document.getElementsById('auto-container')
+const autoContainer = document.getElementById("resultadoContainer")
 
-const mostrarResultados = (mensaje) =>{
-    autoContainer.innerHTML += `<p>${mensaje}</p>`
+const mostrarResultados = (mensaje) => {
+  autoContainer.innerHTML += `<p>${mensaje}</p>`
 }
 
-class Vehiculo{
-    #marca
-    #modelo
-    #color
-    #fabricacion
-    #cilindrada
+class Vehiculo {
+  #marca
+  #modelo
+  #color
+  #fabricacion
+  #cilindrada
 
-    constructor(marca, modelo, color, fabricacion, cilindrada){
-        this.#marca = marca
-        this.#modelo = modelo
-        this.#color = color
-        this.#fabricacion = fabricacion
-        this.#cilindrada = cilindrada
-    }
+  constructor(marca, modelo, color, fabricacion, cilindrada) {
+    this.#marca = marca
+    this.#modelo = modelo
+    this.#color = color
+    this.#fabricacion = fabricacion
+    this.#cilindrada = cilindrada
+  }
 
-    get marca(){
-        return this.#marca
-    }
+  mostrarDatos() {
+    const datos = `-Marca: ${this.#marca}<br>
+                       -Modelo: ${this.#modelo}<br>
+                       -Color: ${this.#color}<br>
+                       -Fabricación: ${this.#fabricacion}<br>
+                       -Cilindrada: ${this.cilindrada}<br>`
+    mostrarResultados(datos)
+  }
 
-    set marca(value){
-        this.#marca = value
-    }
+  arrancar(){
+    const arrancar = `El vehículo ${this.marca} ${this.modelo} ha arrancado`
+    mostrarResultados(arrancar)
+  }
 
-    get modelo(){
-        return this.#modelo
-    }
+  frenar(){
+    const frenar = `El vehículo ${this.marca} ${this.modelo} ha parado`
+    mostrarResultados(frenar)
+  }
 
-    set modelo(value){
-        this.#modelo = value
-    }
+  acelerar(velocidad){
+    const acel = `El vehículo ${this.marca} ${this.modelo} ha acelerado hasta los ${velocidad}km/h`
+    mostrarResultados(acel)
+  }
 
-    get color(){
-        return this.#color
-    }
+  get marca() {
+    return this.#marca
+  }
 
-    set color(value){
-        this.#color = value
-    }
+  set marca(value) {
+    this.#marca = value
+  }
 
-    get fabricacion(){
-        return this.#fabricacion
-    }
+  get modelo() {
+    return this.#modelo
+  }
 
-    set fabricacion(value){
-        this.#fabricacion = value
-    }
+  set modelo(value) {
+    this.#modelo = value
+  }
 
-    get cilindrada(){
-        return this.#cilindrada
-    }
+  get color() {
+    return this.#color
+  }
 
-    set cilindrada(value){
-        this.#cilindrada = value
-    }
+  set color(value) {
+    this.#color = value
+  }
+
+  get fabricacion() {
+    return this.#fabricacion
+  }
+
+  set fabricacion(value) {
+    this.#fabricacion = value
+  }
+
+  get cilindrada() {
+    return this.#cilindrada
+  }
+
+  set cilindrada(value) {
+    this.#cilindrada = value
+  }
 }
+
+const vehiculo1 = new Vehiculo('BMW', 'CLK', 'Rojo', 1992, 2400)
+
+vehiculo1.mostrarDatos()
+vehiculo1.acelerar(140)
+vehiculo1.arrancar()
+vehiculo1.frenar()
